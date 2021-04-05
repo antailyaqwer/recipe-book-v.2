e8a3068314f99b2eb1c28ef7a385ad3eb9713098
antailyaqwer <antailyaqwer@disroot.org>
2021-04-05 12:40:39 +0700

    Change project structure

--- app/src/main/java/org/antailyaqwer/recipebookv2/domain/activities/MainActivity.kt
+++ app/src/main/java/org/antailyaqwer/recipebookv2/domain/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package org.antailyaqwer.recipebookv2
+package org.antailyaqwer.recipebookv2.domain.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.antailyaqwer.recipebookv2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
